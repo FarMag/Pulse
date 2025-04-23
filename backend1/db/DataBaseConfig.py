aeaab663d2834b26b1653be7434a80cf4fd080af
1n4mat1k <90967147+1n4mat1k@users.noreply.github.com>
2025-04-23 23:22:24 +0300

    Add files via upload

---
+++ backend1/db/DataBaseConfig.py
@@ -0,0 +1,9 @@
+class DataBaseConfig:
+    def __init__(self):
+        # self.servername = '192.168.1.4'
+        # self.servername = 'localhost'
+        self.servername = '127.0.0.1'
+        self.username = 'root'
+        self.password = ''
+        # self.databasename = 'testDB'
+        self.databasename = 'pulse_db'
---
+++ backend1/run.py
@@ -0,0 +1,79 @@
+# from backend1.app.auth.auth_run import app as auth_app
+# # from app.product.product_run import app as product_app
+# from threading import Thread
+#
+# def run_auth():
+#     auth_app.run(host='192.168.1.4', port=8001, debug=False)
+#     # auth_app.run(host='0.0.0.0', port=8001, debug=True)
+#
+# # def run_product():
+# #     product_app.run(host='192.168.1.4', port=8002, debug=True)
+#
+# def run_all():
+#     auth_thread = Thread(target=run_auth)
+#     # product_thread = Thread(target=run_product)
+#
+#     auth_thread.start()
+#     # product_thread.start()
+#
+#     auth_thread.join()
+#     # product_thread.join()
+#
+# if __name__ == '__main__':
+#     run_all()  # Запускаем сразу все модули при запуске run.py
+
+
+
+
+
+
+
+
+
+# from app.auth.auth_run import app as auth_app
+# from multiprocessing import Process
+#
+# def run_auth():
+#     auth_app.run(host='192.168.1.4', port=8001, debug=True)
+#
+# if __name__ == '__main__':
+#     auth_process = Process(target=run_auth)
+#     auth_process.start()
+#     auth_process.join()  # Это ожидает завершения процесса, если необходимо
+
+
+
+
+
+
+
+from app.auth.auth_run import app as auth_app
+from app.test.test_run import app as test_app
+from multiprocessing import Process
+import time
+
+def run_auth():
+    auth_app.run(host='192.168.1.4', port=8001, debug=True, use_reloader=False)  # Отключаем авто-перезапуск
+
+def run_test():
+    test_app.run(host='192.168.1.4', port=8002, debug=True, use_reloader=False)  # Отключаем авто-перезапуск
+
+if __name__ == '__main__':
+    auth_process = Process(target=run_auth)
+    test_process = Process(target=run_test)
+
+    auth_process.start()
+    test_process.start()
+
+    # Ожидаем завершения процессов
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        auth_process.terminate()
+        test_process.terminate()
+        auth_process.join()
+        test_process.join()
+
+
+
