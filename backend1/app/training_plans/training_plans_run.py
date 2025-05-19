f199840f72180a2aa5ca013cb914076b30c53f3c
1n4mat1k <90967147+1n4mat1k@users.noreply.github.com>
2025-05-20 02:25:29 +0300

    Add files via upload

---
+++ backend1/app/training_plans/training_plans_run.py
@@ -0,0 +1,18 @@
+import os
+
+from flask import Flask
+from backend1.app.training_plans.routes import (refresh_token_bp, check_token_bp, get_user_personal_training_plan_bp, get_all_training_plan_bp)
+
+app = Flask(__name__)
+# app = Flask(__name__, static_url_path='/static', static_folder=os.path.join(os.getcwd(), 'food_images'))
+
+from backend1.db.DataBaseConfig import DataBaseConfig
+db_config = DataBaseConfig()
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{db_config.username}:{db_config.password}@{db_config.servername}/{db_config.databasename}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.register_blueprint(get_user_personal_training_plan_bp, url_prefix='/api')
+app.register_blueprint(get_all_training_plan_bp, url_prefix='/api')
+
+if __name__ == '__main__':
+    app.run(host=db_config.host, port=8006, debug=True)
---
+++ backend1/run.py
@@ -0,0 +1,65 @@
+from app.auth.auth_run import app as auth_app
+from app.test.test_run import app as test_app
+from app.progress.progress_run import app as progress_app
+from app.nutrition.nutrition_run import app as nutrition_app
+from app.recipes.recipes_run import app as recipes_app
+from app.training_plans.training_plans_run import app as training_plans_app
+from multiprocessing import Process
+from backend1.db.DataBaseConfig import DataBaseConfig
+import time
+
+db_config = DataBaseConfig()
+
+def run_auth():
+    auth_app.run(host=db_config.host, port=8001, debug=True, use_reloader=False)  # Отключаем авто-перезапуск
+
+def run_test():
+    test_app.run(host=db_config.host, port=8002, debug=True, use_reloader=False)  # Отключаем авто-перезапуск
+
+def run_progress():
+    progress_app.run(host=db_config.host, port=8003, debug=True, use_reloader=False)
+
+def run_nutrition():
+    nutrition_app.run(host=db_config.host, port=8004, debug=True, use_reloader=False)
+
+def run_recipes():
+    recipes_app.run(host=db_config.host, port=8005, debug=True, use_reloader=False)
+
+def run_training_plans():
+    training_plans_app.run(host=db_config.host, port=8006, debug=True, use_reloader=False)
+
+if __name__ == '__main__':
+    auth_process = Process(target=run_auth)
+    test_process = Process(target=run_test)
+    progress_process = Process(target=run_progress)
+    nutrition_process = Process(target=run_nutrition)
+    recipes_process = Process(target=run_recipes)
+    training_plans_process = Process(target=run_training_plans)
+
+    auth_process.start()
+    test_process.start()
+    progress_process.start()
+    nutrition_process.start()
+    recipes_process.start()
+    training_plans_process.start()
+
+    # Ожидаем завершения процессов
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        auth_process.terminate()
+        test_process.terminate()
+        progress_process.terminate()
+        nutrition_process.terminate()
+        recipes_process.terminate()
+        training_plans_process.terminate()
+
+        auth_process.join()
+        test_process.join()
+        progress_process.join()
+        nutrition_process.join()
+        recipes_process.join()
+        training_plans_process.join()
+
+
