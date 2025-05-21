2809669e0ce92115ae1660236a92d2e5905a8f93
1n4mat1k <90967147+1n4mat1k@users.noreply.github.com>
2025-05-22 00:16:25 +0300

    Delete backend1 directory

--- backend1/SecretData.py
+++
@@ -1,5 +0,0 @@
-secretKey = "C6Jhsma1L"  # Замените на ваш секретный ключ
-refreshSecretKey = "YourRefreshSecretKey"  # Замените на ваш секретный ключ для refresh токенаACCESS_TOKEN_EXPIRY = 15
-accessTokenExpiry = 6000 # Время жизни access токена в секундах (например, 15 секунд)
-refreshTokenExpiry = 12000 # Время жизни refresh токена в секундах (например, 30 секунд)
-emailEncryptionKey = "cEYjRzgPbJ46kZzOdwoS2f4mXGKtAlJtkYmJENqp0Vw="  # 32 байта base64
